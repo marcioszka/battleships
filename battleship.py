ec11b19d74f4c204b0c9e8420905d82e29a7656e
Zachiel <ezehiel182@gmail.com>
2022-11-12 13:17:59 +0100

    Implemented placing move on board Improved code readability

--- battleship.py
+++ battleship.py
@@ -6,7 +6,7 @@ from string import ascii_uppercase
 from time import sleep
 
 
-class Constants:
+class Constants:  # pylint: disable=[too-few-public-methods]
     """Globally accessed constants."""
 
     SHIP_TYPES: dict[str, list[str]] = {
@@ -40,7 +40,7 @@ class Constants:
 
 
 def generate_board_size(selected_size: int) -> None:
-    """Populate game constants with required values."""
+    """Populate game constants with size adjusted values."""
     Constants.COORDS_TRANSLATION = dict(
         zip(ascii_uppercase[:selected_size], range(selected_size)))
     Constants.VALID_COORDINATES = [row+str(column) for column in
@@ -83,11 +83,23 @@ def get_board_size() -> int:
     return selected_size
 
 
+def confirm_placement():
+    """Ask user for confirmation of ship placement."""
+    pass
+
+
+def remove_ship():
+    """Remove placed ship from player board."""
+    pass
+
+
 def get_user_coords() -> str:
+    """Ask user for coordinates."""
     pass
 
 
 def get_ship_direction() -> str:
+    """Ask user for ship placement plane."""
     pass
 
 
@@ -108,38 +120,51 @@ def get_ship_type() -> str:
 
 
 def validate_coords() -> bool:
+    """Check if input coordinates are withing board bounds."""
     pass
 
 
-def normalize_coords(user_coords: str) -> str:
+def normalize_coords(raw_coords: str) -> str:
+    """Convert user input into a format used in coordinates translation."""
     pass
 
 
-def translate_coords(user_coords: str) -> tuple[int, int]:
+def translate_coords(raw_coords: str) -> tuple[int, int]:
     """Convert user input into a format used in the game."""
     converted: list[int] = [
-        int(Constants.COORDS_TRANSLATION[user_coords[0]]),
-        int(user_coords[1::]) - 1]
+        int(Constants.COORDS_TRANSLATION[raw_coords[0]]),
+        int(raw_coords[1::]) - 1]
     return converted[0], converted[1]
 
 
-def get_empty_board(board_size: int) -> list[str]:
+def check_for_valid_move(defender_visible_board: list[list[str]],
+                         converted_coords: tuple[int, int]) -> bool:
+    """Check if user defined coordinates are a valid move."""
+    row, column = converted_coords[0], converted_coords[1]
+    return (defender_visible_board[row][column]
+            in ["O", "X"])
+
+
+def get_empty_board(board_size: int) -> list[list[str]]:
+    """Create new playable board."""
+    return [["O"]*board_size]*board_size
+
+
+def display_board(player_board: list[list[str]]) -> str:
+    """Convert iterable board into multi line string."""
     pass
 
 
-def display_board(game_board: list[str]) -> None:
-    pass
-
-
-def check_ship_proximity(game_board: list[str],
-                         user_coords: tuple[int, int],
+def check_ship_proximity(player_board: list[list[str]],
+                         converted_coords: tuple[int, int],
                          ship_type: list[str],
                          ship_direction: str) -> bool:
-    """Check if attempt to place a ship has enough space."""
+    """Check if ship placement attempt has enough space."""
     pass
 
 
 def waiting_screen(wait_message: str) -> None:
+    """Display message during placement phase when switching players."""
     pass
 
 # input1 = input("Press any key")
@@ -158,6 +183,7 @@ def clear_terminal() -> None:
 
 
 def boards_side_by_side() -> str:
+    """Display both players visible board versions side by side."""
     pass
 # DICE = [
 #     ("-----",
@@ -188,21 +214,55 @@ def whose_turn_is_it(turn_counter: int) -> str:
     return "Player 2" if turn_counter % 2 == 0 else "Player 1"
 
 
-def check_for_hit(game_board: list[str],
-                  user_coords: tuple[int, int]
-                  ) -> list[str]:
-    pass  # rename later
-
-
-def attempt_feedback(hit_miss_sunk: str) -> None:
+def check_if_sunk():
+    """Confirm that a ship has been sunk."""
     pass
 
 
-def get_winner(game_board: list[str]) -> None:
+def place_move_on_board(defender_visible_board: list[list[str]],
+                        defender_hidden_board: list[list[str]],
+                        converted_coords: tuple[int, int]
+                        ) -> tuple[list[list[str]], list[list[str]]]:
+    """Place attack coordinates result on player boards."""
+    row, column = converted_coords[0], converted_coords[1]
+    hidden_board = defender_hidden_board.copy()
+    visible_board = defender_visible_board.copy()
+    if hidden_board[row][column] == "X":
+        if check_if_sunk():
+            move_attempt_feedback("sunk")
+            hidden_board[row][column] = "S"
+            visible_board[row][column] = "S"
+        else:
+            move_attempt_feedback("hit")
+            hidden_board[row][column] = "H"
+            visible_board[row][column] = "H"
+    elif hidden_board[row][column] == "O":
+        move_attempt_feedback("miss")
+        hidden_board[row][column] = "M"
+        visible_board[row][column] = "M"
+    return hidden_board, visible_board
+
+
+def move_attempt_feedback(hit_miss_sunk: str) -> None:
+    """User feedback based on his move attempt."""
+    pass
+
+
+def check_for_winner(player_board: list[list[str]]) -> tuple[bool, int]:
+    """Check if a player won after his last move."""
+    pass
+
+
+def get_winner(player_board: list[list[str]],
+               turn: int) -> None:
+    """Display a message which player won."""
     pass
 
 
 if __name__ == "__main__":
+    # Testing and execution purpose
+
+    # main()
     # get_ship_type()
     # print(COORDS_TRANSLATION)
     # print(translate_coords("J2138"))
@@ -212,4 +272,5 @@ if __name__ == "__main__":
     # get_board_size()
     # print(Constants.VALID_COORDINATES)
     # print(Constants.COORDS_TRANSLATION)
+    print(get_empty_board(10))
     pass
